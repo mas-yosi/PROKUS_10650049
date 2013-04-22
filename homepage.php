01c7d116abd1c6a221d9ad6bc74ef0e3cb94965b
R Yosi Nurrahman <r.masyosi@gmail.com>
2013-04-23 02:57:32 +0700

    Create homepage.php

---
+++ homepage.php
@@ -0,0 +1,35 @@
+<?php
+?>
+<h2>Anak Islam Anak Berprestasi</h2>
+            
+            <div class="service_box float_l">
+                 
+                 <div class="service_image">
+                    <img src="images/icon_01.png" alt="image 1" />
+                 </div>
+                 <div class="service_text">
+                     <h5>Majalah Anak Islam</h5>
+                     <p>Alasan saya untuk menambah wawasan seputar dunia islam terhadap anak didik dengan menambah informasi dari berbagai sudut pandang islam dan kisah-kisah Nabi pada majalah anak islam</p>
+                     <div class="button"><a href="services.php">details</a></div>
+                 </div>
+                 
+            </div>
+            
+            <div class="service_box float_r">
+            
+                <div class="service_image">
+                    <img src="images/icon_02.png" alt="image 2" />
+                 </div>
+                 <div class="service_text">
+                     <h5>Fashion Anak Islam</h5>
+                     <p>Menambah berbagai ketrampilan busana dan gaya anak islam yang modern dan islamic.</p>
+                     <div class="button"><a href="services.php">details</a></div>
+                 </div>
+                
+            </div>
+            
+            <div class="cleaner_h40"></div>
+            
+            <h2>Bagaimana memulainya?</h2>
+            <p>Orang tua wajib dan penting untuk mendidik anak <a href="" target="_parent">karena anak akan berkembang jika orang tuanya berkembang.</a> Jadi setiap orang tua, harus memperhatikan<a href="" target="_parent"> setiap gerak gerik anak.</a> <a href="" target="_blank">Agar bisa menetukan langkah yang terbaik untuk anak.</a> <a href="" target="_blank"></a></p>
+  		<p>Nah, inilah website yang akan kami sajikan untuk masa depan anak Anda, dengan dunia islamic dan berkembang untuk menjadi pintar dan mengerti akan sejarah dunia islam. Marilah anak-anak kalian pasti bisa menjadi hebat dengan agama yang kuat</p> 
